0aa6499ce7eacff7a5420ba899f497824c9cd0cd
GJ <gjschoutsen@tutanota.com>
2022-02-16 08:56:59 +0100

    changed flow of website. landing page is post page. added basic navbar and some color

--- alienhunters/routes/index.js
+++ alienhunters/routes/index.js
@@ -1,8 +1,22 @@
 const router = require("express").Router();
+const Abduction = require("../models/Abduction.model");
+const isLoggedOut = require("../middleware/isLoggedOut");
+const isLoggedIn = require("../middleware/isLoggedIn");
 
 /* GET home page */
+
 router.get("/", (req, res, next) => {
-  res.render("index", req.session.user);
+
+  Abduction
+    .find()
+    .populate("reporter")
+    .then( abductionsFromDB => {
+      res.render("index", {abductions: abductionsFromDB});
+    })
+    .catch(err => console.log("no posts found in DB", err));
 });
 
+
+
 module.exports = router;
+
