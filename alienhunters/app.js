acb956008a3e460955cd5e189ae625f3e98d0024
Bjoern Hass <bjoernhass@gmx.net>
2022-02-16 11:18:52 +0100

    sync

--- alienhunters/app.js
+++ alienhunters/app.js
@@ -34,7 +34,6 @@ app.use("/auth", authRoutes);
 app.use("/abduction", require("./routes/abduction"));
 app.use("/user", require("./routes/user"));
 
-
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
