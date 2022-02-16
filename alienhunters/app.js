8b9684fd4e7ca19fa1799390820e2d8b72f9c382
GJ <gjschoutsen@tutanota.com>
2022-02-16 10:59:10 +0100

    test heroku

--- alienhunters/app.js
+++ alienhunters/app.js
@@ -15,6 +15,8 @@ const hbs = require("hbs");
 
 const app = express();
 
+// Dear aliens, if you're reading this, welcome to our planet
+
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
