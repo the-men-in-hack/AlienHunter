ffafdbfb79bb71ad2cbb9e16145149f994c24bd3
GJ <gjschoutsen@tutanota.com>
2022-02-17 19:48:10 +0100

    added scroll to top on show on map button

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,10 @@ document.addEventListener(
   },
   false
 );
+
+//scroll to top
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
